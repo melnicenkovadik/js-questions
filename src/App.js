7a41b34a97c136390d3ad36484855c8a535b2cbd
melnicenkovadik <melnicenkovadik@gmail.com>
2022-05-21 18:12:52 +0200

    release

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ function App() {
     return (
         <div className="App">
             <div>
-                <a href="">
+                <a href="https://github.com/melnicenkovadik/js-questions/blob/main/src/Questions.md">
                     React.js, Веб - технологии, JavaScript,
                 </a>
             </div>
             <div>
-                <a href="">
-                    HTML,Web-development
+                <a href="https://github.com/melnicenkovadik/js-questions/blob/main/src/frontend.md">
+                    Веб-разработка, HTML
                 </a>
             </div>
             <div>
-                <a href="">
+                <a href="https://github.com/melnicenkovadik/js-questions/blob/main/src/frontend-two.md">
                     Javascript,CSS
                 </a>
             </div>
